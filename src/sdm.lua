b5645821165774bdc9a66c21c0dbab37027e8f83
Benjamin Tamasi <halftome@gmail.com>
2024-03-19 20:08:38 +0100

    finished data page 1

--- src/sdm.lua
+++ src/sdm.lua
@@ -1,14 +1,20 @@
 -- ANT+ Stride Based Speed and Distance Message
-local proto               = Proto("antplus_sdm_message", "ANT+ SDM Message")
+local proto                   = Proto("antplus_sdm_message", "ANT+ SDM Message")
 
-local page                = ProtoField.uint8("antplus_sdm_message.page", "Data Page Number", base.HEX)
-local time                = ProtoField.float("antplus_sdm_message.time", "Sensor Time (s)")
-local cumulative_distance = ProtoField.float("antplus_sdm_message.cumulative_distance", "Cumulative Distance (m)")
+local page                    = ProtoField.uint8("antplus_sdm_message.page", "Data Page Number", base.HEX)
+local time                    = ProtoField.float("antplus_sdm_message.time", "Sensor Time (s)")
+local cumulative_distance     = ProtoField.float("antplus_sdm_message.cumulative_distance", "Cumulative Distance (m)")
+local instantaneous_speed     = ProtoField.float("antplus_sdm_message.Instantaneous_speed", "Instantaneous Speed (m/s)")
+local cumulative_stride_count = ProtoField.uint8("antplus_sdm_message.cumulative_stride_count", "Cumulative Stride Count")
+local update_latency          = ProtoField.float("antplus_sdm_message.update_latency", "Update Latency (s)")
 
-proto.fields              = {
+proto.fields                  = {
     page,
     time,
     cumulative_distance,
+    instantaneous_speed,
+    cumulative_stride_count,
+    update_latency,
 }
 
 function proto.dissector(buffer, pinfo, tree)
@@ -34,6 +40,18 @@ function proto.dissector(buffer, pinfo, tree)
             cumulative_distance, buffer(3, 2),
             buffer(3, 1):uint() + bit.rshift(buffer(4, 1):uint(), 4) / 16
         )
+
+        -- Instantaneous Speed
+        subtree:add(
+            instantaneous_speed, buffer(4, 2),
+            bit.band(buffer(4, 1):uint(), 0x0F) + buffer(5, 1):uint() / 256
+        )
+
+        -- Cumulative Stride Count
+        subtree:add(cumulative_stride_count, buffer(6, 1))
+
+        -- Update Latency
+        subtree:add(update_latency, buffer(7, 1), buffer(7, 1):uint() / 32)
     elseif pageNum == 0x02 then
         -- decode data page #2
     end
